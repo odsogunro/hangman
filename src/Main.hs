ea519fd5e34cf0a2178264630e76eaa8ed7a539c
odsogunro <dami.kazeem@gmail.com>
2016-06-04 15:26:26 -0400

    added code up to renderPuzzlerChar

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,20 @@ import System.Random (randomRIO)
 
 type WordList = [String]
 
+{-
+String: the word to be guessed
+[Maybe Char]: the characters filled in 
+Char: the characters guessed to present
+
+instance Class Data where
+	func = 
+-}
+data Puzzle = Puzzle String [Maybe Char] [Char]
+instance Show Puzzle where
+    show (Puzzle _ discovered guessed) = 
+        (intersperse ' ' $ fmap renderPuzzleChar discovered)
+        ++ " guessed so far: " ++ guessed 
+
 allWords :: IO WordList
 allWords = do
             dict <- readFile "data/dict.txt"
@@ -32,13 +46,40 @@ gameWords = do
 
 randomWord :: WordList -> IO String
 randomWord wl = do
-                 randomIndex <- randomRIO ((-) (length wl) (length wl), (-) (length wl) 1)
+                 randomIndex <- randomRIO (0, (-) (length wl) 1)
                  return $ wl !! randomIndex
 
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
 
+{-
+> toLower 'A'
+'a'
+> all isJust [Just 'a', Just 'b']
+True
+> all isJust [Just 'a', Nothing]
+False
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle st = Puzzle str mch ch
+-}
+freshPuzzle :: String -> Puzzle
+freshPuzzle st = Puzzle str mch ch
+                 where str = map toLower st
+                       mch = map (const Nothing) str
+                       ch  = []
+
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle str _ _) ch = elem ch str
+
+alreadyGueesed ::  Puzzle -> Char -> Bool
+alreadyGueesed (Puzzle _ _ guessedChars) ch = elem ch guessedChars
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing = '_'
+renderPuzzleChar (Just c)  = c
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    putStrLn "hangman game..."
